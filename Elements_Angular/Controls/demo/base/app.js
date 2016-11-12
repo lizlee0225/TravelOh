efa895a6857cedabbe9ab3f477a6f653b50c895c
sandeeppal1991 <dragoondranzertigerclaw@gmail.com>
2016-11-12 15:07:02 -0800

    Project Structure
    App Project structure added

---
+++ Elements_Angular/Controls/demo/base/app.js
@@ -0,0 +1,33 @@
+﻿
+
+(function(){
+'use strict';
+	var AppController = function ($http,$scope){
+	var self=this;
+		$http.get("/Controls/demo/components.json").then(function(response){
+			$scope.data = response.data;
+		});
+	}
+
+	angular
+		.module('app', [
+		
+					
+		'header'
+		  ,
+		
+		
+					
+		'footerel'
+		  ,
+		
+		
+					
+		'vx.grid.modules'
+		 		
+		
+		
+		])
+		.controller('AppController',['$http','$scope',AppController])
+
+})();
